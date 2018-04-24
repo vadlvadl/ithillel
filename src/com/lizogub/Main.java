5ab7ef5bd4ad261d1f58db3a7dab608ce11ab3cf
Vadim Lizogub <vadlvadl789@gmail.com>
2018-04-24 09:30:38 +0300

    Initial commit

---
+++ src/com/lizogub/Main.java
@@ -0,0 +1,19 @@
+package com.lizogub;
+
+// psvm
+// sout
+// Shift дважды - поиск класса
+// Ctrl + Shift + F - поиск по всему проекту
+// Selenium
+
+public class Main {
+    static int todayDay = 23;
+
+    public static void printTodayDay(){
+        System.out.println(todayDay);
+    }
+
+    public static void main(String[] args) {
+        printTodayDay();
+    }
+}
